9a243b1aef9d310f26517df1959803f6e857a1ac
Sapthasagar H <hsapthasagar@zeomega.com>
2021-12-23 17:01:52 +0530

    onlinetest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HtmlComponent } from './html/html.component';
+import { CssComponent } from './css/css.component';
+import { JavascriptComponent } from './javascript/javascript.component';
+import { TypescriptComponent } from './typescript/typescript.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MainComponent } from './main/main.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { ChangeBgDirective } from './change-bg.directive';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HtmlComponent,
+    CssComponent,
+    JavascriptComponent,
+    TypescriptComponent,
+    WelcomeComponent,
+    MainComponent,
+    HeaderComponent,
+    ChangeBgDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   FormsModule,
+   ReactiveFormsModule,
+   HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
